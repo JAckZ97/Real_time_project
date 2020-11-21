e1239b46b2e7a63d8d60c35bc1e26888143b1fe7
JAckZ97 <zzj1353832196@gmail.com>
2020-11-20 22:02:42 -0500

    sample code for shared memory

--- main.cpp
+++ main.cpp
@@ -1,50 +1,47 @@
-#include <stdio.h>
-#include <sys/json.h>
-
+#include <iostream>
+#include <string>
+#include <array>
 using namespace std;
+bool flag {true};
+array<double, 8> n {0};
 
-int main(){}
+class sharedMemory{
 
-//sample code for QNX IDE Json encode
-//https://www.qnx.com/developers/docs/7.0.0/#com.qnx.doc.json/topic/manual/encoder_sample.html
-bool prepare_file_info(char *str, int max_finfo_size)
-{
-    // In this example, we hardcode the field settings but often, they would be input
-    // by a user or come from another source (e.g., a file or program)
-    int id = 76;
-    int type = 4;
-    long long size = 99;
-    const char *path = "/var/dhclient/leases";
+   public:
+   void access(){
 
-    json_encoder_t *enc = json_encoder_create();
+      // if writeArray():
+      //    flag = false
+      //    writeArray()
+      //    flag = true
+      // else if readArray():
+      //    flag = false
+      //    readArray()
+      //    flag = true
+      // else:
+      //    flag = true
+      
+      writeArray(3, 2.0);
+      readArray();
+   }
 
-    // Write the 'id' value as a separate field in the top-level object, then write the
-    // 'path', 'type', and 'size' fields in a nested object, 'info'.
-    json_encoder_start_object(enc, NULL);
-    json_encoder_add_int(enc, "id", id);
-    json_encoder_start_object(enc, "info");
-    json_encoder_add_string(enc, "path", path);
-    json_encoder_add_int(enc, "type", type);
-    json_encoder_add_int_ll(enc, "size", size);
-    json_encoder_end_object(enc);
-    json_encoder_end_object(enc);
+   private:
+   void readArray(){
+      for (int j = 0; j < n.size(); j++ ){
+         cout << "n[" << j << "] = " << n[j] << endl;
+      }
+   }
 
-    // For error checking, we don't need to check the return value of each function
-    // since any error will cause the subsequent json_encoder_buffer() call to fail. 
-    // Common errors are mismatched object or array start and end calls (which are 
-    // programming errors) and out-of-memory errors.
-    json_encoder_error_t status = json_encoder_get_status(enc);
-    
-    // If everything above has succeeded, json_encoder_get_status() will return 
-    // JSON_ENCODER_OK
-    if ( status != JSON_ENCODER_OK ) {
-        printf("Data preparation failed\n");
-        return false;
-    }
+   private:
+   void writeArray(int index, double value){
+      n[index] = value;
+   }
+};
 
-    // Write the JSON data into the string space provided by the caller
-    snprintf(str, max_finfo_size, "JSON:%s\n", json_encoder_buffer(enc));
-    json_encoder_destroy(enc);
-    
-    return true;
+int main () { 
+   
+   sharedMemory mem;
+   mem.access();
+
+   return 0;
 }
